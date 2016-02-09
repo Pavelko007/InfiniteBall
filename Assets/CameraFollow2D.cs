a8d4210f6de286769f0cf55ff773d40441cd7a2b
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-09 20:23:23 +0200

    add CameraFollow2D script

---
+++ Assets/CameraFollow2D.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
