e2c7c0e05ef47c2f98904d90091c31ddfa9d8028
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-09 19:40:11 +0200

    add FallingPlatform sctipt

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField] private float fallDelay;
+    [SerializeField] private int numJumps;
+    [SerializeField] private bool infiniteJumps = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (infiniteJumps) return;
+
+        StartCoroutine(Fall());
+    }
+
+    IEnumerator Fall()
+    {
+        yield return  new WaitForSeconds(fallDelay);
+        rb2d.isKinematic = false;
+    }
+}
