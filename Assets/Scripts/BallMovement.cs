5494210e8093e79c36a96542365e790412f5729e
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-09 19:16:46 +0200

    add jumping ball

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace InfiniteBall
+{
+    public class BallMovement : MonoBehaviour
+    {
+        [SerializeField] private float horSpeed = 5;
+        [SerializeField] private float vertSpeed = 1;
+
+        private Rigidbody2D rb;
+        private bool isGrounded;
+        private float horAxis;
+        private const int VertSpeedMult = 100;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            Vector2 newVelocity = rb.velocity;
+            newVelocity.x = horAxis * horSpeed;
+            rb.velocity = newVelocity;
+        }
+
+        void Update()
+        {
+            horAxis = Input.GetAxis("Horizontal"); 
+
+            if (Input.GetButtonDown("Jump") &&
+                isGrounded)
+            {
+                rb.AddForce(Vector2.up * vertSpeed * rb.mass * VertSpeedMult);
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision2D)
+        {
+            isGrounded = true;
+        }
+
+        void OnCollisionExit2D(Collision2D collision2D)
+        {
+            isGrounded = false;
+        }
+    }
+}
