a7252291c8dd8f97cd174d7c42235af76796c613
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-10 14:24:25 +0200

    spawn new platforms until rightmost is invisible

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,26 +1,44 @@
 ﻿using UnityEngine;
 using InfiniteBall;
+using InfiniteBall.Extentions;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject platformPrefab;
-    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Transform platformPrefab;
+    [SerializeField] private Transform ballPrefab;
+    private Transform lastPlatform;
+    private Sprite platformSprite;
 
-	// Use this for initialization
+    // Use this for initialization
 	void Awake ()
 	{
-	    Instantiate(platformPrefab, Vector3.zero, Quaternion.identity);
+	    SpawnBall();
 
-	    var platformSprite = platformPrefab.GetComponent<SpriteRenderer>().sprite;
-	    float platformWidth = platformSprite.bounds.size.x;
+	    lastPlatform = Instantiate(platformPrefab, Vector3.zero, Quaternion.identity) as Transform;
 
-	    var ballTransform = Instantiate(ballPrefab, new Vector3(platformWidth / 2, 5, 0), Quaternion.identity) as GameObject;
-
-        Camera.main.GetComponent<Camera2DFollow>().target = ballTransform.transform;
+	    SpawnPlatforms();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    // Update is called once per frame
+    void Update () {
+	    SpawnPlatforms();
 	}
+
+    private void SpawnBall()
+    {
+        platformSprite = platformPrefab.GetComponent<SpriteRenderer>().sprite;
+        float platformWidth = platformSprite.bounds.size.x;
+        Transform ballTransform = Instantiate(ballPrefab, new Vector3(platformWidth / 2, 5, 0), Quaternion.identity) as Transform;
+
+        Camera.main.GetComponent<Camera2DFollow>().target = ballTransform;
+    }
+
+    private void SpawnPlatforms()
+    {
+        while ( lastPlatform.GetComponent<SpriteRenderer>().IsVisibleFrom(Camera.main))
+        {
+            Vector3 nextPos = lastPlatform.position + Vector3.right * platformSprite.bounds.size.x * 1.5f;
+            lastPlatform = Instantiate(platformPrefab, nextPos, Quaternion.identity) as Transform;
+        }
+    }
 }
