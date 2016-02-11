a54e27f15908771f59968689654805b70b7eca75
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-11 15:22:37 +0200

    coins are collectable

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using InfiniteBall.Pooling;
+using UnityEngine;
+
+namespace InfiniteBall
+{
+    public class Coin : MonoBehaviour {
+
+        void OnCollisionEnter2D(Collision2D coll)
+        {
+            GameObjectUtil.Destroy(gameObject);
+        }
+    }
+}
